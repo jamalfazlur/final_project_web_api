1126747b07f89eaae49ae920a49083fd4719f653
jamalfazlur <fazlur.jamal@gmail.com>
2019-02-18 00:44:01 +0700

    module cart user: done

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,27 +1,49 @@
 const conn = require('../database');
 
 module.exports = { 
+
     addtocart: (req,res) => {
-        var newBarang = {
-            username: req.body.username,
-            isbn: req.body.isbn,
-            judul: req.body.judul,
-            harga: req.body.harga,
-            berat: req.body.berat,
-            jumlah_beli: req.body.jumlah_beli,
-            total_harga: req.body.total_harga
-        }
+        var { username, isbn, judul, harga, berat, jumlah_beli, total_berat, total_harga} = req.body;
+
+        var newBarang = { username, isbn, judul, harga, berat, jumlah_beli, total_berat, total_harga };
+
         var sql = `INSERT INTO keranjang SET ? ;`;
         conn.query(sql, newBarang, (err, result) => {
             if(err) throw err;
             res.send(result);
         })
     },
+
     getlistcart: (req, res) => {
-        var sql = `SELECT K.username, K.isbn, K.judul, K.harga, K.jumlah_beli, K.total_harga, B.gambar FROM keranjang K
+        var sql = `SELECT K.username, K.isbn, K.judul, K.harga, K.jumlah_beli, K.total_harga, K.berat, K.total_berat, B.gambar 
+                    FROM keranjang K
                     JOIN buku B 
                     ON K.isbn = B.isbn
                     WHERE username='${req.body.username}';`;
+        conn.query(sql, (err, result) => {
+            if(err) throw err;
+            console.log(sql);
+            console.log(result)
+            res.send(result);
+        })
+    },
+    editlistcart: (req, res) => {
+        var { username, isbn, jumlah_beli, total_berat, total_harga } = req.body;
+
+        var sql = `UPDATE keranjang SET jumlah_beli = ${jumlah_beli}, total_berat = ${total_berat}, total_harga = ${total_harga}
+                    WHERE username = '${username}' AND isbn = '${isbn}';`;
+        conn.query(sql, (err, result) => {
+            if(err) throw err;
+            console.log(sql);
+            console.log(result)
+            res.send(result);
+        })
+    },
+    deleteitemcart: (req,res) => {
+        var { username, isbn } = req.body;
+        
+        var sql = `DELETE FROM keranjang WHERE username = '${username}' AND isbn = '${isbn}';`;
+        
         conn.query(sql, (err, result) => {
             if(err) throw err;
             console.log(sql);
--- routers/transactionRouter.js
+++ routers/transactionRouter.js
@@ -4,5 +4,7 @@ const { transactionController } = require('../controllers');
 
 router.post('/addtocart', transactionController.addtocart);
 router.post('/getlistcart', transactionController.getlistcart);
+router.post('/editlistcart', transactionController.editlistcart);
+router.post('/deleteitemcart', transactionController.deleteitemcart);
 
 module.exports = router;
